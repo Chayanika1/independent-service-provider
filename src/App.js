7a2d7cb840053f7dc2faa4adecac111539fda867
chayanika chakraborty <2016chayanika@gmail.com>
2022-04-16 20:27:57 +0530

    something done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import CheckOut from './Components/CheckOut/CheckOut';
+import Registration from './Components/Registration/Registration';
+import Login from './Components/Login/Login';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header> </Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/Home" element={<Home></Home>}></Route>
+        <Route path="/CheckOut" element={<CheckOut></CheckOut>}></Route>
+        <Route path="/Registration" element={<Registration></Registration>}></Route>
+        <Route path="/Login" element={<Login></Login>}></Route>
+        <Route path="/About" element={<About></About>}></Route>
+      </Routes>
+
     </div>
   );
 }
