7918e9ccbe9cd1adcbfd59724527ffec14b56e3d
chayanika chakraborty <2016chayanika@gmail.com>
2022-04-17 10:50:52 +0530

    social login added

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -36,16 +36,16 @@ const Registration = () => {
                 <div class="mb-3">
                     <h1>Please Register</h1>
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input onBlur={handelEmail} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input onBlur={handelEmail} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input onBlur={handelPassword} type="password" class="form-control" id="exampleInputPassword1"/>
+                    <input onBlur={handelPassword} type="password" class="form-control" id="exampleInputPassword1" required/>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                    <input onBlur={handelConfirmPassword} type="password" class="form-control" id="exampleInputPassword1"/>
+                    <input onBlur={handelConfirmPassword} type="password" class="form-control" id="exampleInputPassword1" required/>
                 </div>
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
--- src/Components/SocialLogin/SocialLogin.js
+++ src/Components/SocialLogin/SocialLogin.js
@@ -3,7 +3,11 @@ import React from 'react';
 const SocialLogin = () => {
     return (
         <div>
-            
+            <div class="d-grid gap-2">
+                <button class="btn btn-primary" type="button">Button</button>
+
+            </div>
+
         </div>
     );
 };
